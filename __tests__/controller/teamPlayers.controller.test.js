39b0e090b048171cc8fae48893aaa29d7b29e9d8
William VanDolah <william.vandolah@gmail.com>
2020-07-18 16:07:24 -0500

    added tests for teamPlayerController

---
+++ __tests__/controller/teamPlayers.controller.test.js
@@ -0,0 +1,145 @@
+const { create, search } = require('../../src/controller/teamPlayers.controller');
+const { setResponse } = require('../../src/helper');
+let { createTeamPlayerRecord, searchTeamPlayerRecord } = require('../../src/service/teamPlayers.service');
+
+const setSearchResponse = (results) => {
+  return {
+    Items: results,
+    Count: results.length,
+    ScannedCount: results.length,
+  };
+};
+
+jest.mock('../../src/service/teamPlayers.service', () => {
+  return {
+    searchTeamPlayerRecord: jest.fn().mockReturnThis(),
+    createTeamPlayerRecord: jest.fn().mockReturnThis(),
+  };
+});
+describe('teamPlayers.controller', () => {
+  const testTeams = [
+    {
+      teamId: 'statusTeam1',
+      userId: 'userId1',
+    },
+    {
+      teamId: 'statusTeam1',
+      userId: 'userId2',
+    },
+    {
+      teamId: 'statusTeam1',
+      userId: 'userId3',
+    },
+    {
+      teamId: 'statusTeam2',
+      userId: 'userId1',
+    },
+    {
+      teamId: 'statusTeam2',
+      userId: 'userId2',
+    },
+  ];
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  describe('when creating  teamPlayer', () => {
+    test('it saves correct when given needed information', async () => {
+      const body = {
+        userId: 'testUserId',
+        teamId: 'testTeamId',
+        players: [
+          {
+            name: 'testName',
+          },
+        ],
+      };
+      const event = {
+        body: JSON.stringify(body),
+      };
+      const actual = await create(event);
+      expect(actual).toEqual(setResponse(201, body, body));
+    });
+    test('it returns status code 400 when not given required information', async () => {
+      const body = {
+        userId: 'testUserId2',
+        players: [
+          {
+            name: 'testName',
+          },
+        ],
+      };
+      const event = {
+        body: JSON.stringify(body),
+      };
+      const actual = await create(event);
+      expect(actual).toEqual(setResponse(400, { error: 'All needed information not provided' }, body));
+    });
+    test('it returns status code 500 when unable to save', async () => {
+      const body = {
+        userId: 'testUserId2',
+        teamId: 'testTeamId',
+        players: [
+          {
+            name: 'testName',
+          },
+        ],
+      };
+      const event = {
+        body: JSON.stringify(body),
+      };
+      createTeamPlayerRecord.mockImplementationOnce(() => {
+        throw new Error('Test Error');
+      });
+      const actual = await create(event);
+      expect(actual).toEqual(setResponse(500, { error: 'Test Error' }, body));
+    });
+  });
+
+  describe('when searching teamPlayer', () => {
+    test('it finds correct when given valid teamId and userId', async () => {
+      const event = { queryStringParameters: testTeams[0], body: JSON.stringify('') };
+      searchTeamPlayerRecord.mockImplementationOnce(() => {
+        return testTeams[0];
+      });
+      const actual = await search(event);
+      const expected = setResponse(200, testTeams[0], testTeams[0]);
+      expect(JSON.parse(actual.body)).toEqual(JSON.parse(expected.body));
+      expect(actual.statusCode).toBe(expected.statusCode);
+    });
+    test('it finds all teams when given valid userId', async () => {
+      const searchTerms = { userId: testTeams[0].userId };
+      const event = { queryStringParameters: searchTerms, body: JSON.stringify('') };
+      searchTeamPlayerRecord.mockImplementationOnce(() => {
+        return [testTeams[0], testTeams[3]];
+      });
+      const actual = await search(event);
+
+      const expected = setResponse(200, [testTeams[0], testTeams[3]], searchTerms);
+      expect(JSON.parse(actual.body)).toEqual(JSON.parse(expected.body));
+      expect(actual.statusCode).toBe(expected.statusCode);
+    });
+    test('it returns 400 status when no userId', async () => {
+      const searchTerms = { userIds: 'invalidKey' };
+      const event = { queryStringParameters: searchTerms, body: JSON.stringify('') };
+      const actual = await search(event);
+
+      const expected = setResponse(400, { error: 'UserId is required' }, searchTerms);
+      expect(JSON.parse(actual.body)).toEqual(JSON.parse(expected.body));
+      expect(actual.statusCode).toBe(expected.statusCode);
+    });
+
+    test('it returns 500 status when save fails', async () => {
+      const searchTerms = { userId: testTeams[0].userId };
+      const event = { queryStringParameters: searchTerms, body: JSON.stringify('') };
+      searchTeamPlayerRecord.mockImplementationOnce(() => {
+        throw new Error('Test Error');
+      });
+      const actual = await search(event);
+
+      const expected = setResponse(500, { error: 'Test Error' }, searchTerms);
+      expect(JSON.parse(actual.body)).toEqual(JSON.parse(expected.body));
+      expect(actual.statusCode).toBe(expected.statusCode);
+    });
+  });
+});
